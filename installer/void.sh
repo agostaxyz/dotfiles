5d0c44a9564089750110b2c7b820f2267647666e
Luca Agosta <me@agosta.dev>
2020-05-23 17:01:58 -0300

    Update void.sh

--- installer/void.sh
+++ installer/void.sh
@@ -4,34 +4,35 @@
 function install () {
     # Allow multilib and nonfree repo.
     sudo xbps-install -Suy void-repo-nonfree void-repo-multilib
-    # Update repos and install must-have Software.
+    # Update repos
     sudo xbps-install -Suy
-    sudo xbps-install -Suy git vim make NetworkManager python inetutils nodejs cmake htop neofetch libpulseaudio curl xcb-util xcb-util-keysyms xcb-util-wm inetutils xtools xorg xinit libX11 rxvt-unicode alsa-lib neovim zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting
 
-    # adding needed files
+    # Must have software
+    sudo xbps-install -Suy git curl NetworkManager neovim make python inetutils nodejs cmake htop libpulseaudio curl xcb-util xcb-util-keysyms xcb-util-wm inetutils xtools alsa-lib qbittorrent noto-fonts ttf-roboto ttf-croscore  exiftool
+    
+    # Auxiliary software
+    sudo xbps-install -Suy firefox surfshark-vpn spotify neofetch zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting lutris
+    # Not yet available on Package Manager
+    # discord
+}
+
+function cdesktop () {
+    sudo xbps-install -Suy sxhkd xinit libxcb lxappearance dmenu conky tint2 dzen2 bspwm polybar xorg xinit libX11 rxvt-unicode
+
+    # Configuring needed files
     echo 'sxhkd &' >> ~/.Xinitrc
     echo 'exec bspwm' >> ~/.Xinitrc
     echo 'polybar main &' >> ~/.config/bspwm/bspwmrc
-
-    # Changing file mode
     chmod +x /home/$USER/.config/bspwm/bspwmrc
     chmod +x /home/$USER/.config/sxhkd/sxhkdrc
     chmod +x git_workspace/dotfiles/private/script.sh
 }
 
-function desktop () {
-    sudo xbps-install -Suy sxhkd xinit libxcb lxappearance dmenu conky tint2 dzen2 bspwm polybar 
-    install
-}
-
-function productivity () {
-    sudo xbps-install -Suy discord
-    install
-}
-
 function all () {
-    sudo xbps-install -Suy steam 
-    desktop
+    sudo xbps-install -Suy blender krita steam
+    # Not yet available on Package Manager
+    # DaVinci-resolve firefox-developer-edition github-desktop surfshark-vpn
+    cdesktop
     full-productivity
-    install
 }
+install
